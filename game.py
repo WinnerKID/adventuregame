25ae9b02ba3cf78b47ec8c12d9d0097bc36cc349
WinnerKID <richy.ronkerboy@gmail.com>
2023-07-18 18:01:48 -0400

    more story

--- game.py
+++ game.py
@@ -55,8 +55,8 @@ while True:
         if choice == "1":
             print()
             print_with_typing("You draw your bow and shoot an arrow at the harpies.\n"
-                              "The arrow finds its mark, striking down one of the harpies.\n"
-                              "The remaining harpies scatter in fear, giving you an opportunity to escape.")
+                              "The arrow finds its mark, striking down one of the monster.\n"
+                              "The remaining foes scatter in fear, giving you an opportunity to escape.")
             print("")
             print("You successfully fend off the harpies with your bow and continue your journey.")
             print()
@@ -107,4 +107,77 @@ while True:
             continue
 
 
-print("~You look around the room and see a lady with a big hat in the middle of the room.~")
+print_with_typing("You spot a lady with a big smile in the middle of the room")
+print('''/////////////\\\\
+(((((((((((((( \\\\
+))) ~~      ~~  (((
+((( (*)     (*) )))
+)))     <       (((
+((( '\______/`  )))
+)))\___________/(((
+       _) (_
+      / \_/ \
+     /(     )\
+    // )___( \\
+    \\(     )//
+     (       )
+      |  |  |
+       | | |
+       | | |
+      _|_|_|_''')
+print("")
+print_with_typing("You walk closer towards her cautiously")
+time.sleep(1)
+print_with_typing("Her grin makes you feel a bit uneasy, making you strike a question at her\n")
+
+
+while True:
+    askher = input("Ask her who she is: ")
+
+    if askher.strip() == "Who are you?":
+        print_with_typing("I am the messenger.")
+        time.sleep(1)
+        print_with_typing("In your head: WELL THAT EXPLAINS NOTHING")
+        break
+
+    else:
+        print_with_typing(". . .")
+        continue
+
+print("~Another question pops into your head~")
+
+while True:
+    whereami = input("Ask her where you are: ")
+
+    if whereami.strip() == "Where am I?":
+        print_with_typing("God's dungeon.")
+        time.sleep(2)
+        print_with_typing(f"{name}: Did god put me in here?")
+        print_with_typing("Lady: . . .")
+        break
+
+    else:
+        print_with_typing(". . .")
+        continue
+
+print_with_typing("~Dead silence in the room~")
+time.sleep(2)
+
+print_with_typing("She's of no use to you now.\rYou draw your bow out and shoot aiming at her head")
+print("~SwwwoooOOOOsshhhHH~")
+time.sleep(1)
+
+print("It passed through her body...\n")
+print_with_typing("Is she a ghost?\n")
+
+print("~Her eyes stare back at you~")
+print('''(⊙‿⊙)''')
+
+print_with_typing("Freaked out, you pull out another arrow aiming for her head again\n")
+print("Before you can even shoot")
+time.sleep(1)
+print_with_typing("She screams: RIIIIIAAAAAAAAAAAAAAAAIIIEEEEEEEEEEEHHHHHHH\n")
+print("You immediately cover your ears due to the loud noise\r~It's too much for you to handle~\rYou collapse just as she finishes her loud shriek")
+
+
+
